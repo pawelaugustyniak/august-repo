cf35e1291ba200827c9b1fdc63d858283aba1330
Augustyniak Pawel <rb21776G@raiffeisen.pl>
2016-08-10 17:24:06 +0200

    adding date testing project

---
+++ lekcja6/Exercise1/src/main/java/FileReader.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.util.Date;
+import java.util.Map;
+import java.util.Scanner;
+
+public class FileReader {
+
+    Map<Date, String> holidaysDates(String inputFile) {
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(new File(inputFile));
+        } catch (FileNot)
+    }
+}
