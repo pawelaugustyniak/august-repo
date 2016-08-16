7a92a90db4534adb7898502e282c0a09e074f9f5
Augustyniak Pawel <rb21776G@raiffeisen.pl>
2016-08-16 10:27:34 +0200

    working project - dates

--- lekcja6/Exercise-dates/src/main/java/HolidayChecker.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDate;
-        import java.util.List;
-
-class HolidayChecker {
-
-    boolean ifDayIsAHoliday(List<LocalDate> holidays, LocalDate date) {
-        return holidays.contains(date);
-    }
-}
